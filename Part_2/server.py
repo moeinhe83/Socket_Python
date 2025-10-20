00e9a30aab57e930dc8e2f7553a791c088cefe8a
moeinhe83 <moeinheshmati1383@gmail.com>
2025-10-20 19:00:44 +0330

    Create Server In server.py File

--- Part_2/server.py
+++ Part_2/server.py
@@ -10,12 +10,36 @@ def handle_client(client_socket, client_addr):
         while True:
             request = client_socket.recv(1024).decode("utf-8")
             if request.lower() == 'close':
-                client_socket.send('Closed'.encode("utf-8"))
+                client_socket.send('[*] Closed'.encode("utf-8"))
                 break
             print(f'[*] Received = {request}')
-            client_socket.send('Accepted'.encode("utf-8"))
+            client_socket.send('[*] Accepted'.encode("utf-8"))
     except Exception as e:
-        print(f'Error = {e}')
+        print(f'[*] Error = {e}')
     finally:
         client_socket.close()
         print(f'{client_addr[0]}:{client_addr[1]}')
+
+
+def start_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((ip_server, port_server))
+        server.listen()
+        print(f'[*] Listen On {ip_server}:{port_server}')
+        while True:
+            client_socket, client_addr = server.accept()
+            print(
+                f'[*] Accepted  Connection From {client_addr[0]}:{client_addr[1]}')
+            thread = threading.Thread(
+                target=handle_client,
+                args=(client_socket, client_addr)
+            )
+            thread.start()
+    except Exception as e:
+        print(f'[*] Error = {e}')
+    finally:
+        server.close()
+
+
+start_server()
