7c318b0d88494fa5bfd374fd95f2392407aecf43
moeinhe83 <moeinheshmati1383@gmail.com>
2025-10-22 19:17:50 +0330

    Add tcp_scan Function

--- Part_3/port_scanner.py
+++ Part_3/port_scanner.py
@@ -1,2 +1,13 @@
 import socket
 import sys
+
+
+def tcp_scan(ip, start_port, end_port):
+    for port in range(start_port, end_port + 1):
+        try:
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if not tcp.connect_ex((ip, port)):
+                print(f'[*] {ip}:{port} Open')
+                tcp.close()
+        except Exception:
+            pass
