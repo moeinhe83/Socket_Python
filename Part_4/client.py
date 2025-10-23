88f01313fbf5683c5dc7a64ff7eaa0ddcc7e2a18
moeinhe83 <moeinheshmati1383@gmail.com>
2025-10-23 20:17:45 +0330

    Create client.py In client.py File

--- Part_4/client.py
+++ Part_4/client.py
@@ -1,1 +1,6 @@
 import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
+    f.connect(('127.0.0.1', 8005))
+    f.send(bytes("moeinit.com", "utf-8"))
+    print(str(f.recv(1024), 'utf-8'))
